962f590d487da40364aacec654348f5e15e75f83
girishkolte2000 <girishkolte2000@gmail.com>
2024-01-16 17:56:56 +0530

    employee middleware updated

--- api/employee/authenticate.middleware.js
+++ api/employee/authenticate.middleware.js
@@ -1,10 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
     return res.status(401).json({ success: 0, message: 'Unauthorized: No token provided' });
   }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ success: 0, message: 'Unauthorized: Invalid token' });
+  }  
 
   jwt.verify(token, process.env.secretkey, (err, decoded) => {
     if (err) {
