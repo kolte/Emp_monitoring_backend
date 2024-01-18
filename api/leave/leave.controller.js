4baa664acc2895a659a3af5262217c2eaa43318f
Shital Jasani <girishkolte2000@gmail.com>
2024-01-18 18:08:55 +0530

    Add leave API done

---
+++ api/leave/leave.controller.js
@@ -0,0 +1,22 @@
+const { createOrUpdateLeave } = require("./leave.service");
+
+module.exports = {
+  createLeave: (req, res) => {
+    const data = req.body;
+
+    createOrUpdateLeave(data, (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database operation error",
+        });
+      }
+
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+};
