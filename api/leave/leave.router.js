71021867df06ba478f5d5b91a7d80bd851276c10
Shital Jasani <girishkolte2000@gmail.com>
2024-01-23 18:10:33 +0530

    create leave type get api and update em_employee_attendance_details flow

--- api/leave/leave.router.js
+++ api/leave/leave.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { createLeave } = require("./leave.controller");
+const { getLeaveType } = require("./leave.controller");
 const authenticate = require("./leave.middleware");
 
 router.post("/", authenticate, createLeave);
+router.get("/", getLeaveType);
 
 module.exports = router;
