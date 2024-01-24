bee08d792b00f5061970086afe8eaaa7b35b1884
Shital Jasani <girishkolte2000@gmail.com>
2024-01-24 18:02:47 +0530

    update em_employee_attendance_details structure and changes in query

--- api/leave/leave.service.js
+++ api/leave/leave.service.js
@@ -3,17 +3,17 @@ const pool = require("../../config/database");
 module.exports = {
   createOrUpdateLeave: (data, callback) => {
     const {
-      leave_name,
-      leave_limit,
+      leave_type_id,
       created_at,
       updated_at,
       employee_id,
       leave_reason,
+      days,
     } = data;
 
     // Check if attendance with the same date exists for the employee
     const checkAttendanceQuery =
-      "SELECT * FROM em_leave_type WHERE employee_id = ? AND created_at = ?";
+      "SELECT * FROM em_employee_attendance_details WHERE employee_id = ? AND created_at = ?";
     pool.query(
       checkAttendanceQuery,
       [employee_id, created_at],
@@ -26,13 +26,20 @@ module.exports = {
         if (results.length > 0) {
           // Attendance exists, update the existing record
           const updateAttendanceQuery = `
-          UPDATE em_leave_type 
-          SET leave_name = ?, leave_limit = ?, created_at = ?, updated_at = ?
+          UPDATE em_employee_attendance_details 
+          SET leave_type_id = ?, leave_reason = ?, created_at = ?, updated_at = ?,days=?
           WHERE employee_id = ?`;
 
           pool.query(
             updateAttendanceQuery,
-            [leave_name, leave_limit, created_at, updated_at, employee_id],
+            [
+              leave_type_id,
+              leave_reason,
+              created_at,
+              updated_at,
+              days,
+              employee_id,
+            ],
             (updateError, updateResults) => {
               if (updateError) {
                 console.error(updateError);
@@ -42,40 +49,33 @@ module.exports = {
             }
           );
         } else {
-          // Attendance does not exist, insert a new record with punch_in as current time
-          const insertAttendanceQuery = `
-          INSERT INTO em_leave_type 
-          (leave_name, leave_limit, created_at, updated_at, employee_id) 
-          VALUES (?, ?, ?, ?, ?)`;
+          const insertAttendanceDataQuery = `
+          INSERT INTO em_employee_attendance_details 
+          (employee_id, employee_attendance_id, leave_type_id, approval_user_id, leave_reason,created_at,updated_at,days) 
+          VALUES (?, 2, ?, 1, ?, ?,?,?)`;
 
           pool.query(
-            insertAttendanceQuery,
-            [leave_name, leave_limit, created_at, updated_at, employee_id],
+            insertAttendanceDataQuery,
+            [
+              employee_id,
+              leave_type_id,
+              leave_reason,
+              created_at,
+              updated_at,
+              days,
+            ],
             (insertError, insertResults) => {
               if (insertError) {
                 return callback(insertError);
               }
-              const insertAttendanceDataQuery = `
-          INSERT INTO em_employee_attendance_details 
-          (employee_id, employee_attendance_id, leave_type_id, approval_user_id, leave_reason,created_at,updated_at) 
-          VALUES (?, 2, 19, 1, ?, ?,?)`;
-
-              pool.query(
-                insertAttendanceDataQuery,
-                [employee_id, leave_reason, created_at, updated_at],
-                (insertError, insertResults) => {
-                  if (insertError) {
-                    return callback(insertError);
-                  }
-                  return callback(null, insertError, insertResults);
-                }
-              );
+              return callback(null, insertError, insertResults);
             }
           );
         }
       }
     );
   },
+
   getLeaveTypeData: (callback) => {
     pool.query(`SELECT * FROM em_leave_type`, (error, results, fields) => {
       if (error) {
