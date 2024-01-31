a6a425daf1d4c133b33728e68ee60f864b9fd5cb
Shital Jasani <girishkolte2000@gmail.com>
2024-01-31 12:41:25 +0530

    add active screen field

--- api/filesupload/filesupload.service.js
+++ api/filesupload/filesupload.service.js
@@ -3,8 +3,16 @@ const pool = require("../../config/database");
 module.exports = {
   create: (data, callBack) => {
     pool.query(
-      `INSERT INTO em_employee_attendance_pc_screenshot (employee_id, employee_attendance_id, screenshot_url, screenshot_time) VALUES (?, ?, ?, ?)`,
-      [data.employee_id, data.employee_attendance_id, data.fullUrl, new Date()],
+      `INSERT INTO em_employee_attendance_pc_screenshot (employee_id, employee_attendance_id, screenshot_url,active_screen,mouse_click,keyboard_click, screenshot_time) VALUES (?, ?, ?,?,?,?)`,
+      [
+        data.employee_id,
+        data.employee_attendance_id,
+        data.fullUrl,
+        data.active_screen,
+        data.mouse_click,
+        data.keyboard_click,
+        new Date(),
+      ],
       (error, results, fields) => {
         if (error) {
           console.log(error);
