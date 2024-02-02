268116ee125b47e50160a156a68f529a191cad0a
girishkolte2000 <girishkolte2000@gmail.com>
2024-02-02 18:02:10 +0530

    changes added for resolving bug in app.js

--- api/employee/employee.controller.js
+++ api/employee/employee.controller.js
@@ -1,15 +1,14 @@
-const { create,getUsers } = require("./employee.service");
-
+const { create, getUsers, updateUser } = require("./employee.service");
 
 module.exports = {
   createUser: (req, res) => {
     const body = req.body;
     create(body, (err, results) => {
       if (err) {
-        console.error('Database error:', err); // Log the error for debugging purposes
+        console.error('Database error:', err);
         return res.status(500).json({
           success: 0,
-          message: err.message || 'Database operation error', // Send the specific error message
+          message: err.message || 'Database operation error',
         });
       }
       return res.status(200).json({
@@ -20,15 +19,37 @@ module.exports = {
   },
 
   getUsers: (req, res) => {
-
     getUsers((err, results) => {
       if (err) {
-        return;
+        return res.status(500).json({
+          success: 0,
+          message: 'Database operation error',
+        });
       }
       return res.json({
         success: 1,
         data: results,
       });
     });
-  }
+  },
+
+  updateUserDetails: (req, res) => {
+    const body = req.body;
+    const userId = req.params.id;
+
+    updateUser(userId, body, (err, results) => {
+      if (err) {
+        console.error('Database error:', err);
+        return res.status(500).json({
+          success: 0,
+          message: err.message || 'Database operation error',
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        message: 'Employee details updated successfully',
+        data: results,
+      });
+    });
+  },
 };
