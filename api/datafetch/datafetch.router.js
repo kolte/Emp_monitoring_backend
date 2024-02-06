67804be28b6375c1f30b403aa74b874d4d20b68c
girishkolte2000 <girishkolte2000@gmail.com>
2024-02-06 16:57:33 +0530

    datafetch api added

---
+++ api/datafetch/datafetch.router.js
@@ -0,0 +1,11 @@
+// datafetch.router.js
+const express = require('express');
+const router = express.Router();
+const datafetchController = require('./datafetch.controller');
+
+router.get('/employee-details', datafetchController.getEmployeeDetails);
+router.get('/departments', datafetchController.getDepartments);
+router.get('/jobs', datafetchController.getJobs);
+router.get('/users', datafetchController.getUsers);
+
+module.exports = router;
