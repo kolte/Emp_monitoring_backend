d4230574cbb16f83f0d9b9131a8867e3e6321fb2
girishkolte2000 <girishkolte2000@gmail.com>
2024-02-14 12:44:00 +0530

    Screenshot API

--- api/userReport/userReport.service.js
+++ api/userReport/userReport.service.js
@@ -1,26 +1,49 @@
-const bcrypt = require("bcrypt");
 const pool = require("../../config/database");
 
-module.exports = {
-  getUsers: (employeeId, callback) => {
-   
+exports.getUsers = (employeeId, callback) => {
+  pool.query(`SELECT * FROM em_employee_attendance where employee_id= ?`, employeeId, (error, results, fields) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
+};
 
-    pool.query(`SELECT * FROM em_employee_attendance where employee_id= ?`, employeeId, (error, results, fields) => {
-      if (error) {
-        return callback(error, null);
-      }
-      return callback(null, results);
-    });
+exports.getUsersPunchReport = (employeeId, callback) => {
+  pool.query(`SELECT * FROM em_employee_attendance_punch where employee_id= ?`, employeeId, (error, results, fields) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
+};
 
-  },
+exports.getEmployeeScreenshots = (employeeId, date, callback) => {
+  const query = `
+    SELECT screenshot_url, screenshot_time, active_screen, mouse_click, keyboard_click
+    FROM em_employee_attendance_pc_screenshot
+    WHERE employee_id = ? AND DATE(screenshot_time) = ?
+  `;
+  pool.query(query, [employeeId, date], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
+};
 
-  getUsersPunchReport: (employeeId,callback) => {
-
-    pool.query(`SELECT * FROM em_employee_attendance_punch where employee_id= ?`, employeeId, (error, results, fields) => {
-      if (error) {
-        return callback(error, null);
-      }
-      return callback(null, results);
-    });
-  },
+exports.getLatestFourScreenshots = (employeeId, callback) => {
+  const query = `
+    SELECT screenshot_url, screenshot_time, active_screen, mouse_click, keyboard_click
+    FROM em_employee_attendance_pc_screenshot
+    WHERE employee_id = ?
+    ORDER BY screenshot_time DESC
+    LIMIT 4
+  `;
+  pool.query(query, [employeeId], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
 };
