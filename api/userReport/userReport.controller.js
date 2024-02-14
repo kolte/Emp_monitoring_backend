05b76496735346d0eebf43726d20b2a07c63a094
girishkolte2000 <girishkolte2000@gmail.com>
2024-02-14 12:44:00 +0530

    Screenshot API

--- api/userReport/userReport.controller.js
+++ api/userReport/userReport.controller.js
@@ -1,28 +1,45 @@
-const { getUsers ,getUsersPunchReport} = require("./userReport.service");
+const { getUsers ,getUsersPunchReport,getEmployeeScreenshots, getLatestFourScreenshots } = require("./userReport.service");
 
-module.exports = {
-  getUsers: (req, res) => {
-    const { employeeId } = req.query;
-    getUsers(employeeId,(err, results) => {
-      if (err) {
-        return;
-      }
-      return res.json({
-        success: 1,
-        data: results,
-      });
-    });
-  },
-  getUsersPunchReport: (req, res) => {
-    const { employeeId } = req.query;
-    getUsersPunchReport(employeeId,(err, results) => {
-      if (err) {
-        return;
-      }
-      return res.json({
-        success: 1,
-        data: results,
-      });
-    });
-  },
+exports.getUsers = (req, res) => {
+  const { employeeId } = req.query;
+  getUsers(employeeId, (error, results) => {
+    if (error) {
+      console.error("Error fetching users:", error);
+      return res.status(500).json({ success: 0, message: "Internal Server Error" });
+    }
+    return res.json({ success: 1, data: results });
+  });
+};
+
+exports.getUsersPunchReport = (req, res) => {
+  const { employeeId } = req.query;
+  getUsersPunchReport(employeeId, (error, results) => {
+    if (error) {
+      console.error("Error fetching users punch report:", error);
+      return res.status(500).json({ success: 0, message: "Internal Server Error" });
+    }
+    return res.json({ success: 1, data: results });
+  });
+};
+
+exports.getEmployeeScreenshots = (req, res) => {
+  const { employeeId, date } = req.query;
+  getEmployeeScreenshots(employeeId, date, (error, results) => {
+    if (error) {
+      console.error("Error fetching employee screenshots:", error);
+      return res.status(500).json({ success: 0, message: "Internal Server Error" });
+    }
+    return res.json({ success: 1, data: results });
+  });
+};
+
+exports.getLatestFourScreenshots = (req, res) => {
+  const { employeeId } = req.query;
+  getLatestFourScreenshots(employeeId, (error, results) => {
+    if (error) {
+      console.error("Error fetching latest four screenshots:", error);
+      return res.status(500).json({ success: 0, message: "Internal Server Error" });
+    }
+    return res.json({ success: 1, data: results });
+  });
 };
