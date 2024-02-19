6c729fc107fcaecef8884cbc2717973a3adbaa7b
Shital Jasani <girishkolte2000@gmail.com>
2024-02-19 18:09:33 +0530

    Employee list Query Update

--- api/employeeList/employeeList.service.js
+++ api/employeeList/employeeList.service.js
@@ -3,7 +3,8 @@ const pool = require("../../config/database");
 
 module.exports = {
   getUsers: (callback) => {
-    pool.query(`SELECT * FROM em_employee`, (error, results, fields) => {
+    // pool.query(`SELECT * FROM em_employee`, (error, results, fields) => {
+    pool.query(`SELECT * FROM em_employee e LEFT JOIN em_employee_attendance_pc_screenshot d ON e.id = d.employee_id`, (error, results, fields) => {
       if (error) {
         return callback(error, null);
       }
