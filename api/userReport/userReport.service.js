6cada9d56038e59d500c8e52e782a3748bbe303b
Shital Jasani <girishkolte2000@gmail.com>
2024-02-23 18:26:02 +0530

    update query

--- api/userReport/userReport.service.js
+++ api/userReport/userReport.service.js
@@ -18,6 +18,15 @@ exports.getUsersPunchReport = (employeeId, callback) => {
   });
 };
 
+exports.getTimeReport = (employeeId,emp_date, callback) => {
+  pool.query(`SELECT (SUM(e.mouse_click + e.keyboard_click)) AS mouclick, (SUM(r.total_time)) AS total_time FROM em_employee_attendance_pc_screenshot e JOIN em_employee_attendance_punch r ON e.employee_id = r.employee_id WHERE e.employee_id =? AND DATE(r.punch_in) = ?`, [employeeId, emp_date],(error, results, fields) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
+};
+
 exports.getEmployeeScreenshots = (employeeId, date, callback) => {
   const query = `
     SELECT screenshot_url, screenshot_time, active_screen, mouse_click, keyboard_click
