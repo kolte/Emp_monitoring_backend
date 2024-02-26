fe0898773cda0fad1dd0c18a04f2c8223e458218
Shital Jasani <girishkolte2000@gmail.com>
2024-02-26 12:08:45 +0530

    update env file

--- config/database.js
+++ config/database.js
@@ -9,10 +9,12 @@ const {createPool} = require('mysql2');
 // })
 
 const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: "",
-    database: process.env.MYSQL_DB,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DB,
+    
   });
 
 
