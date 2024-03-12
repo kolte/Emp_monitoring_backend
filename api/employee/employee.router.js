41bc9b823b17d76473af5c938b159ac35cff2158
girishkolte2000 <girishkolte2000@gmail.com>
2024-03-12 14:51:18 +0530

    employee API added

--- api/employee/employee.router.js
+++ api/employee/employee.router.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { createUser, getUsers, updateUserDetails } = require("./employee.controller");
+const { createUser, getUsers, updateUserDetails, deleteUser } = require("./employee.controller");
 const authenticate = require("./authenticate.middleware");
 
 router.get("/", authenticate, getUsers);
 router.post("/", authenticate, createUser);
 router.put("/:id", authenticate, updateUserDetails);
+router.delete("/:id", authenticate, deleteUser);
+
 
 module.exports = router;
