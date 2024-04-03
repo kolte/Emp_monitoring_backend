e229fc7159f71d41f031efeb47cc2819de4e0abc
girishkolte2000 <girishkolte2000@gmail.com>
2024-04-03 10:36:04 +0530

    permission module

--- api/permission/permission.router.js
+++ api/permission/permission.router.js
@@ -2,9 +2,14 @@ const router = require("express").Router();
 const permissionController = require('./permission.controller');
 const authenticate = require("./authenticate.middleware");
 
-router.post('/', permissionController.createPermission);
-router.get('/:id', permissionController.getPermissionById);
-router.put('/:id', permissionController.updatePermission);
-router.delete('/:id', permissionController.deletePermission);
+router.post('/',authenticate, permissionController.createPermission);
+router.get('/:id',authenticate, permissionController.getPermissionById);
+router.put('/:id',authenticate, permissionController.updatePermission);
+router.delete('/:id',authenticate, permissionController.deletePermission);
+router.post('/role-permissions', authenticate, permissionController.createRolePermission);
+router.get('/role-permissions', authenticate, permissionController.getAllRolePermissions);
+router.put('/role-permissions/:id', authenticate, permissionController.updateRolePermission);
+router.delete('/role-permissions/:id', authenticate, permissionController.deleteRolePermission);
+
 
 module.exports = router;
